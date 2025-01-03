df64dd43d8caf698529e271769df03164fe5126f
sameer <sameerbagul2004@gmail.com>
2025-01-03 20:05:32 +0530

    sanity CLI added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,57 @@
-export default function Home() {
+import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { query?: string };
+}) {
+  const query = searchParams.query;
+
+  // Mock posts array
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "Sameer Bagul" },
+      _id: 1,
+      description: "This is a Description",
+      image: "https://th.bing.com/th/id/OIP.nURkWURpGxW7PqroM0dyNgHaJ4?rs=1&pid=ImgDetMain",
+      category: "robots",
+      title: "We Robots",
+    },
+  ];
+
   return (
-  <>
-    <h1 className="heading">Home</h1>
-  </>
+    <>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br />
+          Connect with Entrepreneurs
+        </h1>
+
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote On Pitches, and Get Noticed In Virtual Competitions.
+        </p>
+
+        <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search Results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts.length > 0 ? (
+            posts.map((post) => (
+              <StartupCard key={post._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
+      </section>
+    </>
   );
 }
